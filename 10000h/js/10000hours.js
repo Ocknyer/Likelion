dc105c8eacf320fa70701ff1f95abae94b044486
Ocknyer <107315656+Ocknyer@users.noreply.github.com>
2022-09-14 02:33:44 +0900

    Add files via upload
    CSS 수정 및 반응형 작업

--- 10000h/js/10000hours.js
+++ 10000h/js/10000hours.js
@@ -1,8 +1,15 @@
 const modal = document.getElementById('modal');
 const modalOpenBtn = document.getElementById('btn-go');
+const modalCloseBtn = document.getElementById('btn-exit');
 
 //modal 창 오픈
 modalOpenBtn.addEventListener('click', (event) => {
    event.preventDefault();
    modal.classList.add('active');
+});
+
+//modal 창 닫기
+modalCloseBtn.addEventListener('click', (event) => {
+   event.preventDefault();
+   modal.classList.remove('active');
 });
